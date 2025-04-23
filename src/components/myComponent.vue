f0b7b315ad2901c34d59d45f6f170d87d7809e98
Nicolas Boeuf <nicolas_boeuf@hotmail.fr>
2025-04-23 15:34:14 +0200

    init vue3 boilerplate

---
+++ src/components/myComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="myComponent">
+       <span>Je suis un composant</span>
+    </div>
+  </template>
+  
+  <script>
+  //import store from '@/store'
+  
+  export default {
+    name: 'MyComponent',
+    data(){
+      return {
+      }
+    },
+    props: {
+    },
+    computed: {
+    },
+    methods: {
+    },
+  
+    watch:{
+    },
+  
+    created(){
+    }
+  
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+    
+    @use "../../css/variables.scss";
+    @use "../../css/overload-fonts.css";
+  
+    span{
+      color:green;
+      font-family:"Marianne-Light";
+    }
+  
+    @media (max-width: 728px) {
+      span{
+        color:blue;
+      }    
+    }
+  
+  
+  </style>
+  
