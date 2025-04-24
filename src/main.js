e07cf4feb3a51ecb3617b4fd541da5aa51b37e2d
Nicolas Boeuf <nicolas_boeuf@hotmail.fr>
2025-04-24 15:51:16 +0200

    test iframeresizer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 
 import { createApp } from 'vue'
 import { store } from './store'
+import * as iframeResizer from '@iframe-resizer/child'
+
 import App from './App.vue'
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(store).use(iframeResizer).mount('#app')
